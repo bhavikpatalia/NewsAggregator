9f53a25985f24a9e0b9f72f419d229c86db64b9d
meenavivek <vivek.me@media.net>
2022-08-16 16:21:31 +0530

    Added parser for timesofindia news

--- src/main/java/HttpURLConnection.java
+++ src/main/java/HttpURLConnection.java
@@ -1,4 +1,7 @@
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -43,13 +46,20 @@ public class HttpURLConnection {
             }
             in.close();
 
-            System.out.println(response.toString());
+//            System.out.println(response.toString());
         } else {
             System.out.println("GET request not worked");
         }
+
+        JSONArray json = XML.toJSONObject(response.toString()).getJSONObject("rss").getJSONObject("channel").getJSONArray("item");
+//        System.out.println(json);
 //        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 //                .parse(new InputSource(new StringReader(response.toString())));
-
+        JSONObject jsonArray = json.getJSONObject(0);
+        System.out.println(jsonArray.get("link"));
+        System.out.println(jsonArray.get("title"));
+        System.out.println(jsonArray.get("description").toString().replaceAll("<[^>]*>", ""));
+        System.out.println(jsonArray.get("pubDate"));
     }
 
 
