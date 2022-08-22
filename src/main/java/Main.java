1919d4e4a714bf535ef49303e476f5be673ab123
meenavivek <vivek.me@media.net>
2022-08-22 17:15:52 +0530

    * Changed file structure
    * Added two methods for CosineSimilarity : WithMerging and Without Merging
    
    * Added entity for News
    
    * Added repository and service for newsEntity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,28 @@
 
-import GenerateResponse.HttpURLConnection;
-import NewsGenerator.*;
-import NewsParsers.NewsParser;
-import NewsSources.*;
-import Responses.Response;
+import com.example.NewsAggregator.GenerateResponse.HttpURLConnection;
+import com.example.NewsAggregator.NewsSources.*;
+import com.example.NewsAggregator.Responses.Response;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
+//import com.opencsv.ICSVWriter;
 import org.json.JSONArray;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.NewsAggregator.NewsDataStorage.ReadFromCSVFiles.getDataFromCSVFile;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseException {
+    private static final String CSV_SEPARATOR = "|";
+
+    public Main() throws FileNotFoundException, UnsupportedEncodingException {
+    }
+
+    public static void main(String[] args) throws ParseException, IOException {
         JSONArray response = null;
         try {
             response = HttpURLConnection.sendGET(TheHindu.SCIENCE.getAction());
@@ -22,9 +32,56 @@ public class Main {
             e.printStackTrace();
         }
         Main main = new Main();
-        ScienceNewsGenerator scienceNewsGenerator = new ScienceNewsGenerator();
-//        List<Response> parsedNews = NewsParser.getParsedNews(response);
-        List<Response> parsedNews = scienceNewsGenerator.getAllScienceNews();
-        System.out.println(parsedNews);
+        List<Response> dataFromCSVFile = getDataFromCSVFile("/Users/vivek.me/NewsAggregator/Science.csv");
+
+    }
+
+    public static void writeDataForCustomSeparatorCSV(List<Response> responses, String path)
+    {
+        File file = new File(path);
+
+        try {
+            FileWriter outputfile = new FileWriter(file);
+
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            List<String[]> data = new ArrayList<String[]>();
+            data.add(new String[] { "Id", "Title", "Time", "Description", "Link" });
+            int count = 1;
+            for(Response response : responses){
+                String[] temp = new String[]{Integer.toString(count), response.getTitle(), response.getPubTime().toString(), response.getDescription(), response.getLink()};
+                data.add(temp);
+                count += 1;
+            }
+            writer.writeAll(data);
+
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static void readAllDataAtOnce(String file)
+    {
+        try {
+            FileReader filereader = new FileReader(file);
+
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(1)
+                    .build();
+
+            List<String[]> allData = csvReader.readAll();
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
