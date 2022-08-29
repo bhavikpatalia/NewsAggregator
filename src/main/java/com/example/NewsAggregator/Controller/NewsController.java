a45863782834da5a9fab36c5b528ee1d0d6ec532
Bhavik.Patalia <bhavik.p@media.net>
2022-08-29 12:50:23 +0530

    Added cross origin anotation

--- src/main/java/com/example/NewsAggregator/Controller/NewsController.java
+++ src/main/java/com/example/NewsAggregator/Controller/NewsController.java
@@ -3,13 +3,12 @@ package com.example.NewsAggregator.Controller;
 import com.example.NewsAggregator.Service.NewsServiceImplForCSV;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+
+@CrossOrigin
 @RestController
 public class NewsController {
 
